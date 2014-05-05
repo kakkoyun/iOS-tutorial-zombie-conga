375018e4d86654bb8b0e22122b7f8614d0b7409c
Kemal Akkoyun <kakkoyun@gmail.com>
2014-05-05 09:33:22 +0300

    init.

--- Zombie Conga/KAKMyScene.m
+++ Zombie Conga/KAKMyScene.m
@@ -10,44 +10,19 @@
 
 @implementation KAKMyScene
 
--(id)initWithSize:(CGSize)size {    
-    if (self = [super initWithSize:size]) {
-        /* Setup your scene here */
-        
-        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
-        
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-        
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
-        
-        [self addChild:myLabel];
-    }
-    return self;
-}
-
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    /* Called when a touch begins */
-    
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
-    }
-}
-
--(void)update:(CFTimeInterval)currentTime {
-    /* Called before each frame is rendered */
+- (instancetype)initWithSize:(CGSize)size
+{
+  if (self = [super initWithSize:size]) {
+    self.backgroundColor = [SKColor whiteColor];
+    SKSpriteNode *background = [SKSpriteNode spriteNodeWithImageNamed:@"background"];
+//    background.anchorPoint = CGPointZero;
+//    background.position = CGPointZero;
+    background.anchorPoint = CGPointMake(0.5, 0.5); // Default point.
+    background.position = CGPointMake(self.size.width / 2, self.size.height / 2);
+//    background.zRotation = M_PI / 8;
+    [self addChild:background];
+  }
+  return self;
 }
 
 @end
--- Zombie Conga/KAKViewController.m
+++ Zombie Conga/KAKViewController.m
@@ -11,12 +11,14 @@
 
 @implementation KAKViewController
 
-- (void)viewDidLoad
+- (void)viewWillLayoutSubviews
 {
-    [super viewDidLoad];
-
-    // Configure the view.
-    SKView * skView = (SKView *)self.view;
+  [super viewWillLayoutSubviews];
+  
+  // Configure the view.
+  SKView * skView = (SKView *)self.view;
+  
+  if(!skView.scene){
     skView.showsFPS = YES;
     skView.showsNodeCount = YES;
     
@@ -26,26 +28,31 @@
     
     // Present the scene.
     [skView presentScene:scene];
+  }
 }
 
 - (BOOL)shouldAutorotate
 {
-    return YES;
+  return YES;
 }
 
 - (NSUInteger)supportedInterfaceOrientations
 {
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-        return UIInterfaceOrientationMaskAllButUpsideDown;
-    } else {
-        return UIInterfaceOrientationMaskAll;
-    }
+  if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+    return UIInterfaceOrientationMaskAllButUpsideDown;
+  } else {
+    return UIInterfaceOrientationMaskAll;
+  }
 }
 
 - (void)didReceiveMemoryWarning
 {
-    [super didReceiveMemoryWarning];
-    // Release any cached data, images, etc that aren't in use.
+  [super didReceiveMemoryWarning];
+  // Release any cached data, images, etc that aren't in use.
+}
+
+- (BOOL)prefersStatusBarHidden {
+  return YES;
 }
 
 @end
