e3c5c2504576b060a58f1f35f334a0e79af6b656
Kemal Akkoyun <kakkoyun@gmail.com>
2014-05-05 09:54:15 +0300

    Challenges and end of chapter one.

--- Zombie Conga/KAKMyScene.m
+++ Zombie Conga/KAKMyScene.m
@@ -9,18 +9,30 @@
 #import "KAKMyScene.h"
 
 @implementation KAKMyScene
+{
+  SKSpriteNode *_zombie;
+}
 
 - (instancetype)initWithSize:(CGSize)size
 {
   if (self = [super initWithSize:size]) {
     self.backgroundColor = [SKColor whiteColor];
+    
     SKSpriteNode *background = [SKSpriteNode spriteNodeWithImageNamed:@"background"];
 //    background.anchorPoint = CGPointZero;
 //    background.position = CGPointZero;
     background.anchorPoint = CGPointMake(0.5, 0.5); // Default point.
     background.position = CGPointMake(self.size.width / 2, self.size.height / 2);
 //    background.zRotation = M_PI / 8;
+    NSLog(@"Size: %@", NSStringFromCGSize(background.size));
+    
+    _zombie = [SKSpriteNode spriteNodeWithImageNamed:@"zombie1"];
+    _zombie.position = CGPointMake(100, 100);
+    _zombie.xScale = 2;
+    _zombie.yScale = 2;
+    
     [self addChild:background];
+    [self addChild:_zombie];
   }
   return self;
 }
